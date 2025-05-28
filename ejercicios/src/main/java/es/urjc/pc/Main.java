c5a724c266a659f30693a98ee85bc00918f155e4
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2025-05-28 19:00:56 +0200

    Resubido el ej13

---
+++ ejercicios/src/main/java/es/urjc/pc/Main.java
@@ -0,0 +1,8 @@
+package es.urjc.pc;
+import static es.urjc.etsii.code.concurrency.SimpleConcurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
